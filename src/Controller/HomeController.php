626f03f8149390e1e827da74019fc66a480db615
Simon Charbonnier <simoncharbonnier@orange.fr>
2023-07-21 12:05:18 +0200

    style: tricks list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
+        $tricks = $trickRepository->findAll();
+
+        if (empty($tricks)) {
+            $this->addFlash('danger', 'Il n\'y a pas de figures disponibles.');
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tricks' => $tricks,
         ]);
     }
 }
