174639ea047a6dbb5c60fde7ae56d7d7c8586f84
Simon Charbonnier <simoncharbonnier@orange.fr>
2023-07-22 17:13:49 +0200

    feat: paginator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,23 +4,34 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Trick;
 use App\Repository\TrickRepository;
+use Knp\Component\Pager\PaginatorInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(TrickRepository $trickRepository): Response
+    public function index(TrickRepository $trickRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $tricks = $trickRepository->findAll();
+        $tricks = $trickRepository->findBy([], ['created_at' => 'DESC']);
 
         if (empty($tricks)) {
             $this->addFlash('danger', 'Il n\'y a pas de figures disponibles.');
         }
 
-        return $this->render('home/index.html.twig', [
-            'tricks' => $tricks,
-        ]);
+        $tricks = $paginator->paginate(
+            $tricks,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render(
+            'home/index.html.twig',
+            [
+                'tricks' => $tricks
+            ]
+        );
     }
 }
